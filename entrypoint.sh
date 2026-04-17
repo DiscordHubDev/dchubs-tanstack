8fc2046242ebba527dccadcffdbd54f9d4ed6bad
Mantouisyummy <opcantel@gmail.com>
2026-04-17 20:30:01 +0800

    pointed to correct port

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,8 @@ fi
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
+export PORT=${SERVER_PORT}
+
 bun -v
 
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
